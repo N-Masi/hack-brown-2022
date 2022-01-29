cd2a7e8aca521fae60afc40d029520cc7fdb91f2
N-Masi <nickmasi17@gmail.com>
2022-01-29 12:45:10 -0500

    feat: movable player w/ background

--- gene_house.py
+++ gene_house.py
@@ -1,15 +1,16 @@
 import sys, pygame
 pygame.init()
 
-
-size = width, height = 640, 480
-speed = [2, 2]
+size = width, height = 512, 512
+speed = 3
+velocity = [0, 0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+background = pygame.image.load("background.png")
+player = pygame.image.load("char_walk_right.gif")
+player_rect = player.get_rect()
 clock = pygame.time.Clock()
 
 while 1:
@@ -18,15 +19,32 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                speed[0]*=2
-                speed[1]*=2
+            velocity[0]=0
+            velocity[1]=speed
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            velocity[0]=0
+            velocity[1]=-speed
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            velocity[0]=speed
+            velocity[1]=0
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            velocity[0]=-speed
+            velocity[1]=0
+        else:
+            velocity[0]=0
+            velocity[1]=0
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    if player_rect.left < 0:
+        velocity[0] = max(0, velocity[0])
+    elif player_rect.right > width:
+        velocity[0] = min(0, velocity[0])
+    elif player_rect.top < 0:
+        velocity[1] = max(0, velocity[1])
+    elif player_rect.bottom > height:
+        velocity[1] = min(0, velocity[1])
+    player_rect = player_rect.move(velocity)
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(background, (0,0))
+    screen.blit(player, player_rect)
     pygame.display.flip()
